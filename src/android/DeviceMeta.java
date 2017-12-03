025325e7d5c6683a09bac369205d171297ba5dcf
Mike Brannigan <mike@msxpert.com>
2017-12-02 19:52:06 -0800

    wololo

--- src/android/DeviceMeta.java
+++ src/android/DeviceMeta.java
@@ -5,7 +5,11 @@ import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager.NameNotFoundException;
 import android.net.wifi.WifiManager;
 import android.telephony.TelephonyManager;
+import android.media.AudioManager;
 import android.os.Build;
+import android.os.Process;
+import android.provider.Settings;
+import java.io.File;
 
 import java.util.Formatter;
 
@@ -34,15 +38,40 @@ public class DeviceMeta extends CordovaPlugin {
             
             JSONObject r = new JSONObject();
             r.put("debug", this.isDebug());
+            r.put("development", this.isDevelopment());
+            r.put("root", this.isDeviceRooted());
             r.put("networkProvider", this.getNetworkProvider());
             r.put("ip", this.getIpAddress());
             r.put("manufacturer", this.getManufacturer());
+            r.put("DNDMode",this.getDNDMode());
 
             callbackContext.success(r);
         } else {
             return false;
         }
         return true;
+    }
+
+
+    private boolean isDeviceRooted() {
+        return checkBuildTags() || checkSuperUserApk() || checkFilePath();
+    }
+    private boolean checkBuildTags() {
+        String buildTags = android.os.Build.TAGS;
+        return buildTags != null && buildTags.contains("test-keys");
+    }
+
+    private boolean checkSuperUserApk() {
+        return new File("/system/app/Superuser.apk").exists();
+    }
+
+    private boolean checkFilePath() {
+        String[] paths = { "/sbin/su", "/system/bin/su", "/system/xbin/su", "/data/local/xbin/su", "/data/local/bin/su", "/system/sd/xbin/su",
+                "/system/bin/failsafe/su", "/data/local/su" };
+        for (String path : paths) {
+            if (new File(path).exists()) return true;
+        }
+        return false;
     }
 
     private boolean isDebug() {
@@ -59,6 +88,19 @@ public class DeviceMeta extends CordovaPlugin {
         return false;
     }
 
+
+    private boolean isDevelopment() {
+        if(!Settings.Secure.getString(this.cordova.getActivity().getContentResolver(), Settings.Global.DEVELOPMENT_SETTINGS_ENABLED).equals("0"))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+    }
+
     private String getIpAddress() {
         return Utils.getIPAddress(true);
     }
@@ -66,6 +108,11 @@ public class DeviceMeta extends CordovaPlugin {
     private String getNetworkProvider() {
         TelephonyManager tm = (TelephonyManager) ctx.getSystemService(Context.TELEPHONY_SERVICE);
         return tm.getNetworkOperatorName();
+    }
+    
+    private int getDNDMode() {
+        AudioManager am = (AudioManager) ctx.getSystemService(Context.AUDIO_SERVICE);
+        return am.getRingerMode(); 
     }
 
     private String getManufacturer() {
